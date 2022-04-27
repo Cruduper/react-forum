7aea0439f7db4745eea37ba6da65f86c8aae01b0
Eric Crudup <briefcasemanx@gmail.com>
2022-04-27 10:40:39 -0700

    header implemented

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import PostsContainer from './Posts/PostsContainer';
+import Header from './Header';
+import React from "react";
+
+function App() {
+  return (
+    <React.Fragment>
+      <Header />
+      <PostsContainer />
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/components/Posts/PostsContainer.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default function PostsContainer() {
+  return (
+    <>
+    </>
+  )
+}
